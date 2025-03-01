a5bc20610affb4ce6ad0c2518aa5f2bd1ec7c1eb
Danny Zirko <danny.zirko@gmail.com>
2025-03-01 15:07:37 -0600

    Remove duplicate favicon link tag

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={inter.className}>
         <AuthProvider>
